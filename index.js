02b9fb39960501a13de9c75a5dca275e7dc04cca
Nancy-zitle <nancy.zitle@producepay.com>
2024-04-03 14:17:42 -0600

    [main]: create typeDefs and the person data

---
+++ index.js
@@ -0,0 +1,34 @@
+import { gql } from "apollo-server";
+
+const persons = [
+  {
+    name: "Midu",
+    phone: "034-1234567",
+    street: "Calle Frontend",
+    city: "Barcelona",
+    id: "3d594650-3436-11e9-bc57-8b80ba54c431",
+  },
+  {
+    name: "Youseff",
+    phone: "044-123456",
+    street: "Avenida Fullstack",
+    city: "Mataro",
+    id: "3d599470-3436-11e9-bc57-8b80ba54c431",
+  },
+  {
+    name: "Itzi",
+    street: "Pasaje Testing",
+    city: "Ibiza",
+    id: "3d599471-3436-11e9-bc57-8b80ba54c431",
+  },
+];
+
+const typeDefs = gql`
+  type Person {
+    name: String!
+    phone: String
+    street: String!
+    city: String!
+    id: ID!
+  }
+`;
